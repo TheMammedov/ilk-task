72bef1a77c9f85f573bdc0435f7aa3f20e032d61
TheMammedov <elvin10220@gmail.com>
2024-07-28 14:02:22 +0400

    Ilk task

---
+++ ilktask.dart
@@ -0,0 +1,54 @@
+void main() {
+// Task 1
+
+  var a = ; //Eded daxil edin
+
+  if (a < 0) {
+    print("Bu eded menfidir");
+  } else if (a > 0) {
+    print("Bu eded musbetdir");
+  } else {
+    print("Bu eded 0a beraberdir");
+  }
+
+// Task 2
+  var b = ; //Yas daxil edin
+
+  if (0 <= b && b <= 12) {
+    print("Usaq");
+  } else if (13 <= b && b <= 19) {
+    print("Genc");
+  } else if (20 <= b && b <= 65) {
+    print("Yetkin");
+  } else if (b > 65) {
+    print("Yasli");
+  } else {
+    print("Yasinizi duzgun daxil edin");
+  }
+
+// Task 3
+
+  var c = ; //Temperatur daxil edin
+
+  if (c < 0) {
+    print("Buzlu");
+  } else if (0 <= c && c <= 20) {
+    print("Soyuq");
+  } else if (21 <= c && c <= 30) {
+    print("Isti");
+  } else if (c > 30) {
+    print("Cox isti");
+  } else {
+    print("Duzgun derece daxil edin");
+  }
+
+// Task 4
+
+  int parol = ; //Parol daxil edin
+
+  if (parol == 12345) {
+    print("Duzgun parol");
+  } else {
+    print("Yanlis parol");
+  }
+}
